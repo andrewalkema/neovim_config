a8447abeb35d0791353555e098f5ae03acf0b9b0
Andrew Alkema <andrew.alkema@d2l.com>
2025-08-13 11:26:27 -0400

    Add flash nvim

---
+++ lua/plugins/flash-nvim.lua
@@ -0,0 +1,51 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    modes = {
+      char = {
+        enabled = false,
+      },
+      search = {
+        enabled = true,
+      },
+    },
+    jump = {
+      autojump = true,
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "<Leader>r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote flash",
+    },
+    {
+      "<Leader>R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Flash Treesitter Search",
+    },
+  },
+}
