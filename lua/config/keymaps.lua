b71adceac5da9ff7d145de940f85a576bfa5d30f
Andrew Alkema <aalkema@gmail.com>
2025-11-04 20:25:27 -0500

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,12 @@ vim.keymap.set("n", "<leader>cf", function()
     lsp_format = "fallback",
   })
 end, { desc = "Format current file" })
+
+vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
+
+-- Basic movement keybinds, these make navigating splits easy for me
+vim.keymap.set("n", "<c-j>", "<c-w><c-j>")
+vim.keymap.set("n", "<c-k>", "<c-w><c-k>")
+vim.keymap.set("n", "<c-l>", "<c-w><c-l>")
+vim.keymap.set("n", "<c-h>", "<c-w><c-h>")
